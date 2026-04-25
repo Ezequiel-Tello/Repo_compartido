13d1194f76795d3a834ee05aa8d44e8a30cc3ff6
esgaal <52287449+esgaal@users.noreply.github.com>
2026-04-24 22:39:46 -0300

    Create grupo.py

---
+++ grupo.py
@@ -0,0 +1,4 @@
+#1 Iniciamos con un print de presentacion/saludo
+print("ventas y montos")
+#2 Solicito al usuario un número de ventas a trabajar
+ventas = int(input("Ingrese el número de ventas a trabajar: "))
