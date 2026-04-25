e38af62242abb0c96d853546c205f0c94ed8a656
esgaal <52287449+esgaal@users.noreply.github.com>
2026-04-24 22:39:46 -0300

    Create grupo.py

---
+++ grupo.py
@@ -0,0 +1,9 @@
+""" Se desea desarrollar un programa en Python que permita trabajar con un conjunto de ventas.
+Solicitar al usuario la cantidad de ventas a cargar.
+Cargar un vector (lista) con los montos de cada venta.
+Una vez cargado el vector, recorrerlo y calcular el total de ventas realizadas.
+Mostrar por pantalla el total obtenido. """
+#1 Iniciamos con un print de presentacion/saludo
+print("ventas y montos")
+#2 Solicito al usuario un número de ventas a trabajar
+ventas = int(input("Ingrese el número de ventas a trabajar: "))
