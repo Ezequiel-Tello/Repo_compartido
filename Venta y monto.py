e085b12f6973e80487b014ebb22d2a58417b443d
Jav989 <278969889+Jav989@users.noreply.github.com>
2026-04-27 09:49:21 -0300

    Update Venta y monto.py

--- Venta y monto.py
+++ Venta y monto.py
@@ -1,12 +1,12 @@
 #1 saludamos con un msj de presentacion
 print("montos y ventas")
 #2 solicitamos la cantidad de ventas
-cantidad = int(input("¿cuantas ventas queres ingresar"))
+cantidad = int(input("¿cuantas ventas queres ingresar?: "))
 #3 iniciamos la lista (vector)
 ventas = []
 #4 cargamos el vector con los montos
 for i in range(cantidad):
-    monto = float(input(f"ingrese el monto de la venta{i+1}:"))
+    monto = float(input(f"ingrese el monto de la venta {i+1}: "))
     ventas.append(monto)
 #5 calculamos el total de los montos de cada venta
 total_ventas = 0
@@ -14,3 +14,5 @@ for venta in ventas:
     total_ventas += venta
 #6  mostramos el resultado
 print(f"El total de ventas realizadas es:${total_ventas:.2f}")
+
+
