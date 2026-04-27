09d28cff8e6c5bc81428dcaff952a316ffa560bf
misiliolucas <278969889+Jav989@users.noreply.github.com>
2026-04-27 08:54:01 -0300

    Create Venta y monto.py

---
+++ Venta y monto.py
@@ -0,0 +1,7 @@
+ventas = []
+cantidad = int(input("¿cuantas ventas queres ingresar"))
+for i in range(cantidad):
+    monto = float(input(f"ingrese el monto de la venta{i+1}"))
+    ventas.append(monto)
+
+   
